8fa068ccb392f058fbfcf0e188500438fb3078a2
dltjdska32 <dltjdska32@naver.com>
2025-03-04 16:41:57 +0900

    remove: all file remove

--- src/main/java/test/erp_project/config/SessionConst.java
+++
@@ -1,5 +0,0 @@
-package test.erp_project.config;
-
-public abstract class SessionConst {
-    public static final String LOGIN_USER = "login_user";
-}
--- src/main/java/test/erp_project/dto/leave_dto/LeaveStatus.java
+++
@@ -1,5 +0,0 @@
-package test.erp_project.dto.leave_dto;
-
-public enum LeaveStatus {
-    ACCEPT, REJECT
-}
